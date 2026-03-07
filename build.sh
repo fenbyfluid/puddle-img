c70f87c397e1e65313009131fceb605b9170e5aa
Erin <fenbyfluid@gmail.com>
2026-03-07 13:54:03 +0000

    Build USB rescue image for installation

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ set -eu
 dir=$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")
 IGTOP="$dir/rpi-image-gen"
 
-exec "$IGTOP/rpi-image-gen" build -S "$dir" -c puddle-img.yaml
+"$IGTOP/rpi-image-gen" build -S "$dir" -c puddle-img.yaml
+"$IGTOP/rpi-image-gen" build -S "$dir" -c puddle-rescue.yaml
