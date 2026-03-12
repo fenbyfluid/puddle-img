a65b63b4fe800574fc1a800be9c5e09adeba8fa0
Erin <fenbyfluid@gmail.com>
2026-03-12 22:11:07 +0000

    Add embedded LinMot-Talk package for OOB access

--- build.sh
+++ build.sh
@@ -5,5 +5,19 @@ set -eu
 dir=$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")
 IGTOP="$dir/rpi-image-gen"
 
+BUILD_RESCUE=0
+for arg in "$@"; do
+  if [ "$arg" = "-r" ] || [ "$arg" = "--build-rescue" ]; then
+    BUILD_RESCUE=1
+  fi
+done
+
+# The build gets quite large, avoid a tmpfs /tmp/
+mkdir -p "$dir/work/tmp/"
+export TMPDIR="$dir/work/tmp/"
+
 "$IGTOP/rpi-image-gen" build -S "$dir" -c puddle-img.yaml
-"$IGTOP/rpi-image-gen" build -S "$dir" -c puddle-rescue.yaml
+
+if [ "$BUILD_RESCUE" -eq "1" ]; then
+  "$IGTOP/rpi-image-gen" build -S "$dir" -c puddle-rescue.yaml
+fi
